2d3772888db5614cf562a45d2d8faf3df210a53d
unknown <JoJo@DESKTOP-A0430JU.GCBLTD.COM>
2020-06-12 00:17:45 +0000

    summernote added

--- api/models.py
+++ api/models.py
@@ -34,7 +34,7 @@ class Course(models.Model):
 
 class Topic(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    title = models.CharField(max_length=100, null=False, default='Computer Science', unique=True)
+    title = models.CharField(max_length=100, null=False, default='Computer Science')
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     description = models.CharField(max_length=200, null=False, default='This is is DZCS101')
     pub_date = models.DateTimeField(auto_now_add=True)
--- dApi/settings.py
+++ dApi/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'dApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'damdev',
+        'USER': 'postgres',
+        'PASSWORD': 'http404#k',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
